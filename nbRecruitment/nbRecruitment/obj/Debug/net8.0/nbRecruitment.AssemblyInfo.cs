f54279b92490d89a2255ed2519d4e4e9dac94d25
Kakuwarai <alvarezmarkjearone@gmail.com>
2024-03-07 08:30:12 +0800

    news

--- nbRecruitment/nbRecruitment/obj/Debug/net8.0/nbRecruitment.AssemblyInfo.cs
+++ nbRecruitment/nbRecruitment/obj/Debug/net8.0/nbRecruitment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nbRecruitment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bc232202cb03b8e6308730d996c2be64f8a911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b74e4084d938b75bc192f1662df774824663ed4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("nbRecruitment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nbRecruitment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
